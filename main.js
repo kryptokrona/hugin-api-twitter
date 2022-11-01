dc398f96e17b7205caacf9e0a1608b2b2e9b0576
Teo Lundgren <tgg@telia.com>
2022-11-01 18:52:43 +0100

    Making code more readable and removed some outcommented stuff

--- main.js
+++ main.js
@@ -7,37 +7,37 @@ const {TwitterApi} = require('twitter-api-v2');
 require('dotenv').config();
 const cron = require("node-cron");
 
-const client = new TwitterApi({
+const client = new TwitterApi({ 
     appKey: process.env.APPKEY,
     appSecret: process.env.APPSECRET,
     accessToken: process.env.ACCESSTOKEN,
     accessSecret: process.env.ACCESSSECRET,
-});
+})
 
-async function sendToTwitter(message) {
+async function sendToTwitter(message) { 
     return await client.v2.tweet(message)
-}
-async function sendReplytoTwitter(message, id) {
+} 
+
+async function sendReplytoTwitter(message, id) { 
     return await client.v2.reply(message, id)
-}
+} 
+
 async function getStats() {
     await fetch('https://api.hugin.chat/api/v1/posts')
     .then((response) => {
-        return response.json()
+        return response.json() 
     }).then(async (json) => {
         let postAmount = json.total_items
         postMessage = 'Currently rocking ' + postAmount + ' messages stored in Official Hugin API 🔥' 
         let response = await sendToTwitter(postMessage)
-        // console.log(response) Only for debug
-        let id = response.data.id
-        await fetch('https://api.hugin.chat/api/v1/posts-encrypted')
+        let id = response.data.id 
+        await fetch('https://api.hugin.chat/api/v1/posts-encrypted') 
         .then((response) => {
             return response.json()
         }).then(async (json) => {
             let encryptedAmount = json.total_items
             encryptedMessage = 'We also currently have ' + encryptedAmount + ' encrypted messages in the database'
             let response = await sendReplytoTwitter(encryptedMessage, id)
-            // console.log(response) Only for debug
         })
     })
 }
@@ -45,4 +45,4 @@ async function getStats() {
 cron.schedule("0 12 * * *", function () {
     console.log("Running todays task")
     getStats()
-});
+}) 
